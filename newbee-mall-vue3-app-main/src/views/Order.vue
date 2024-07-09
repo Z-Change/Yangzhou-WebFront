f9bea2ff656fdb8fce0aa16aa22560008dc87032
wutcriminal <1668139313@qq.com>
2024-07-09 17:19:46 +0800

    修改主题色之前的存档

--- newbee-mall-vue3-app-main/src/views/Order.vue
+++ newbee-mall-vue3-app-main/src/views/Order.vue
@@ -11,7 +11,8 @@
 <template>
   <div class="order-box">
     <s-header :name="'我的订单'" :back="'/user'"></s-header>
-    <van-tabs @click-tab="onChangeTab" :color="'#1baeae'" :title-active-color="'#1baeae'" class="order-tab" v-model="state.status">
+    <van-tabs @click-tab="onChangeTab" :color="'#1baeae'" :title-active-color="'#1baeae'"
+      class="order-tab" v-model="state.status">
       <van-tab title="全部" name=''></van-tab>
       <van-tab title="待付款" name="0"></van-tab>
       <van-tab title="待确认" name="1"></van-tab>
@@ -21,27 +22,17 @@
     </van-tabs>
     <div class="content">
       <van-pull-refresh v-model="state.refreshing" @refresh="onRefresh" class="order-list-refresh">
-        <van-list
-          v-model:loading="state.loading"
-          :finished="state.finished"
-          finished-text="没有更多了"
-          @load="onLoad"
-          @offset="10"
-        >
-          <div v-for="(item, index) in state.list" :key="index" class="order-item-box" @click="goTo(item.orderNo)">
+        <van-list v-model:loading="state.loading" :finished="state.finished" finished-text="没有更多了"
+          @load="onLoad" @offset="10">
+          <div v-for="(item, index) in state.list" :key="index" class="order-item-box"
+            @click="goTo(item.orderNo)">
             <div class="order-item-header">
               <span>订单时间：{{ item.createTime }}</span>
               <span>{{ item.orderStatusString }}</span>
             </div>
-            <van-card
-              v-for="one in item.newBeeMallOrderItemVOS"
-              :key="one.orderId"
-              :num="one.goodsCount"
-              :price="one.sellingPrice"
-              desc="全场包邮"
-              :title="one.goodsName"
-              :thumb="$filters.prefix(one.goodsCoverImg)"
-            />
+            <van-card v-for="one in item.newBeeMallOrderItemVOS" :key="one.orderId"
+              :num="one.goodsCount" :price="one.sellingPrice" desc="全场包邮" :title="one.goodsName"
+              :thumb="$filters.prefix(one.goodsCoverImg)" />
           </div>
         </van-list>
       </van-pull-refresh>
@@ -51,11 +42,11 @@
 
 <script setup>
 import { reactive } from 'vue';
-import sHeader from '@/components/SimpleHeader.vue'
-import { getOrderList } from '@/service/order'
-import { useRouter } from 'vue-router'
+import sHeader from '@/components/SimpleHeader.vue';
+import { getOrderList } from '@/service/order';
+import { useRouter } from 'vue-router';
 
-const router = useRouter()
+const router = useRouter();
 const state = reactive({
   status: '',
   loading: false,
@@ -64,110 +55,113 @@ const state = reactive({
   list: [],
   page: 1,
   totalPage: 0
-})
+});
 
 const loadData = async () => {
-  const { data, data: { list } } = await getOrderList({ pageNumber: state.page, status: state.status })
-  state.list = state.list.concat(list)
-  state.totalPage = data.totalPage
+  const {
+    data,
+    data: { list }
+  } = await getOrderList({ pageNumber: state.page, status: state.status });
+  state.list = state.list.concat(list);
+  state.totalPage = data.totalPage;
   state.loading = false;
-  if (state.page >= data.totalPage) state.finished = true
-}
+  if (state.page >= data.totalPage) state.finished = true;
+};
 
 const onChangeTab = ({ name }) => {
   // 这里 Tab 最好采用点击事件，@click，如果用 @change 事件，会默认进来执行一次。
-  state.status = name
-  onRefresh()
-}
+  state.status = name;
+  onRefresh();
+};
 
-const goTo = (id) => {
-  router.push({ path: '/order-detail', query: { id } })
-}
+const goTo = id => {
+  router.push({ path: '/order-detail', query: { id } });
+};
 
 const goBack = () => {
-  router.go(-1)
-}
+  router.go(-1);
+};
 
 const onLoad = () => {
   if (!state.refreshing && state.page < state.totalPage) {
-    console.log(state.page)
-    console.log(state.totalPage)
-    state.page = state.page + 1
+    console.log(state.page);
+    console.log(state.totalPage);
+    state.page = state.page + 1;
   }
   if (state.refreshing) {
     state.list = [];
     state.refreshing = false;
   }
-  loadData()
-}
+  loadData();
+};
 
 const onRefresh = () => {
-  state.refreshing = true
-  state.finished = false
-  state.loading = true
-  state.page = 1
-  onLoad()
-}
+  state.refreshing = true;
+  state.finished = false;
+  state.loading = true;
+  state.page = 1;
+  onLoad();
+};
 </script>
 
 <style lang="less" scoped>
-  @import '../common/style/mixin';
-  .order-box {
-    .order-header {
-      position: fixed;
-      top: 0;
-      left: 0;
-      z-index: 10000;
-      .fj();
-      .wh(100%, 44px);
-      line-height: 44px;
-      padding: 0 10px;
-      .boxSizing();
-      color: #252525;
-      background: #fff;
-      border-bottom: 1px solid #dcdcdc;
-      .order-name {
-        font-size: 14px;
-      }
+@import '../common/style/mixin';
+.order-box {
+  .order-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10000;
+    .fj();
+    .wh(100%, 44px);
+    line-height: 44px;
+    padding: 0 10px;
+    .boxSizing();
+    color: #252525;
+    background: #fff;
+    border-bottom: 1px solid #dcdcdc;
+    .order-name {
+      font-size: 14px;
     }
-    .order-tab {
-      position: fixed;
-      left: 0;
-      z-index: 1000;
-      width: 100%;
-      border-bottom: 1px solid #e9e9e9;
+  }
+  .order-tab {
+    position: fixed;
+    left: 0;
+    z-index: 1000;
+    width: 100%;
+    border-bottom: 1px solid #e9e9e9;
+  }
+  .skeleton {
+    margin-top: 60px;
+  }
+  .content {
+    height: calc(~'(100vh - 70px)');
+    overflow: hidden;
+    overflow-y: scroll;
+    margin-top: 34px;
+  }
+  .order-list-refresh {
+    .van-card__content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
     }
-    .skeleton {
-      margin-top: 60px;
+    .van-pull-refresh__head {
+      background: #f9f9f9;
     }
-    .content {
-      height: calc(~"(100vh - 70px)");
-      overflow: hidden;
-      overflow-y: scroll; 
-      margin-top: 34px;
-    }
-    .order-list-refresh {
-      .van-card__content {
+    .order-item-box {
+      margin: 20px 10px;
+      background-color: #fff;
+      .order-item-header {
+        padding: 10px 20px 0 20px;
         display: flex;
-        flex-direction: column;
-        justify-content: center;
+        justify-content: space-between;
       }
-      .van-pull-refresh__head {
-        background: #f9f9f9;
-      }
-      .order-item-box {
-        margin: 20px 10px;
+      .van-card {
         background-color: #fff;
-        .order-item-header {
-          padding: 10px 20px 0 20px;
-          display: flex;
-          justify-content: space-between;
-        }
-        .van-card {
-          background-color: #fff;
-          margin-top: 0;
-        }
+        margin-top: 0;
       }
     }
   }
+}
 </style>
--- newbee-mall-vue3-app-main/src/views/ProductList.vue
+++ newbee-mall-vue3-app-main/src/views/ProductList.vue
@@ -15,31 +15,26 @@
         <i class="nbicon nbfanhui" @click="goBack"></i>
         <div class="header-search">
           <i class="nbicon nbSearch"></i>
-          <input
-            type="text"
-            class="search-title"
-            v-model="state.keyword"/>
+          <input type="text" class="search-title" v-model="state.keyword" />
         </div>
-        <span class="search-btn" @click="getSearch">搜索</span>
+        <van-button class="search-btn" @click="getSearch">搜索</van-button>
       </header>
-      <van-tabs type="card" color="#1baeae" @click-tab="changeTab" >
+      <van-tabs type="card" color="#1baeae" @click-tab="changeTab">
         <van-tab title="推荐" name=""></van-tab>
         <van-tab title="新品" name="new"></van-tab>
         <van-tab title="价格" name="price"></van-tab>
       </van-tabs>
     </div>
     <div class="content">
-      <van-pull-refresh v-model="state.refreshing" @refresh="onRefresh" class="product-list-refresh">
-        <van-list
-          v-model:loading="state.loading"
-          :finished="state.finished"
-          :finished-text="state.productList.length ? '没有更多了' : '搜索想要的商品'"
-          @load="onLoad"
-          @offset="10"
-        >
+      <van-pull-refresh v-model="state.refreshing" @refresh="onRefresh"
+        class="product-list-refresh">
+        <van-list v-model:loading="state.loading" :finished="state.finished"
+          :finished-text="state.productList.length ? '没有更多了' : '搜索想要的商品'" @load="onLoad"
+          @offset="10">
           <!-- <p v-for="item in list" :key="item">{{ item }}</p> -->
           <template v-if="state.productList.length">
-            <div class="product-item" v-for="(item, index) in state.productList" :key="index" @click="productDetail(item)">
+            <div class="product-item" v-for="(item, index) in state.productList" :key="index"
+              @click="productDetail(item)">
               <img :src="$filters.prefix(item.goodsCoverImg)" />
               <div class="product-info">
                 <p class="name">{{item.goodsName}}</p>
@@ -56,11 +51,11 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { search } from '@/service/good'
-const route = useRoute()
-const router = useRouter()
+import { reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { search } from '@/service/good';
+const route = useRoute();
+const router = useRouter();
 const state = reactive({
   keyword: route.query.keyword || '',
   searchBtn: false,
@@ -73,104 +68,113 @@ const state = reactive({
   totalPage: 0,
   page: 1,
   orderBy: ''
-})
+});
 const init = async () => {
-  const { categoryId } = route.query
+  const { categoryId } = route.query;
   if (!categoryId && !state.keyword) {
-    state.finished = true
+    state.finished = true;
     state.loading = false;
-    return
+    return;
   }
-  const { data, data: { list } } = await search({ pageNumber: state.page, goodsCategoryId: categoryId, keyword: state.keyword, orderBy: state.orderBy })
-  
-  state.productList = state.productList.concat(list)
-  state.totalPage = data.totalPage
+  const {
+    data,
+    data: { list }
+  } = await search({
+    pageNumber: state.page,
+    goodsCategoryId: categoryId,
+    keyword: state.keyword,
+    orderBy: state.orderBy
+  });
+
+  state.productList = state.productList.concat(list);
+  state.totalPage = data.totalPage;
   state.loading = false;
-  if (state.page >= data.totalPage) state.finished = true
-}
+  if (state.page >= data.totalPage) state.finished = true;
+};
 
 const goBack = () => {
-  router.go(-1)
-}
+  router.go(-1);
+};
 
-const productDetail = (item) => {
-  router.push({ path: `/product/${item.goodsId}` })
-}
+const productDetail = item => {
+  router.push({ path: `/product/${item.goodsId}` });
+};
 
 const getSearch = () => {
-  onRefresh()
-}
+  onRefresh();
+};
 
 const onLoad = () => {
   if (!state.refreshing && state.page < state.totalPage) {
-    state.page = state.page + 1
+    state.page = state.page + 1;
   }
   if (state.refreshing) {
     state.productList = [];
     state.refreshing = false;
   }
-  init()
-}
+  init();
+};
 
 const onRefresh = () => {
-  state.refreshing = true
-  state.finished = false
-  state.loading = true
-  state.page = 1
-  onLoad()
-}
+  state.refreshing = true;
+  state.finished = false;
+  state.loading = true;
+  state.page = 1;
+  onLoad();
+};
 
 const changeTab = ({ name }) => {
-  console.log('name', name)
-  state.orderBy = name
-  onRefresh()
-}
+  console.log('name', name);
+  state.orderBy = name;
+  onRefresh();
+};
 </script>
 
 <style lang="less" scoped>
-  @import '../common/style/mixin';
-  .product-list-content {
-    position: fixed;
-    left: 0;
-    top: 0;
+@import '../common/style/mixin';
+.product-list-content {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
+  background: #fff;
+  .category-header {
+    .fj();
     width: 100%;
-    z-index: 1000;
-    background: #fff;
-    .category-header {
-      .fj();
-      width: 100%;
-      height: 50px;
-      line-height: 50px;
-      padding: 0 15px;
-      .boxSizing();
-      font-size: 15px;
-      color: #656771;
-      z-index: 10000;
-      &.active {
-        background: @primary;
+    height: 50px;
+    line-height: 50px;
+    padding: 0 15px;
+    .boxSizing();
+    font-size: 15px;
+    color: #656771;
+    z-index: 10000;
+    &.active {
+      background: @primary;
+    }
+    .icon-left {
+      font-size: 25px;
+      font-weight: bold;
+    }
+    .header-search {
+      display: flex;
+      width: 76%;
+      line-height: 20px;
+      margin: 10px 0;
+      padding: 5px 0;
+      color: #232326;
+      background: #62a6a6;
+      .borderRadius(20px);
+      .nbSearch {
+        padding: 0 5px 0 20px;
+        font-size: 17px;
       }
-      .icon-left {
-        font-size: 25px;
-        font-weight: bold;
+      .search-title {
+        width: 80%;
+        font-size: 12px;
+        color: #666;
+        background: #f7f7f7;
       }
-      .header-search {
-        display: flex;
-        width: 76%;
-        line-height: 20px;
-        margin: 10px 0;
-        padding: 5px 0;
-        color: #232326;
-        background: #F7F7F7;
-        .borderRadius(20px);
-        .nbSearch {
-          padding: 0 5px 0 20px;
-          font-size: 17px;
-        }
-        .search-title {
-          font-size: 12px;
-          color: #666;
-          background: #F7F7F7;
-        }
     }
     .icon-More {
       font-size: 20px;
@@ -187,58 +191,58 @@ const changeTab = ({ name }) => {
     }
   }
 }
-  .content {
-    height: calc(~"(100vh - 70px)");
-    overflow: hidden;
-    overflow-y: scroll; 
-    margin-top: 78px;
-  }
-  .product-list-refresh {
-    .product-item {
-      .fj();
-      width: 100%;
+.content {
+  height: calc(~'(100vh - 70px)');
+  overflow: hidden;
+  overflow-y: scroll;
+  margin-top: 78px;
+}
+.product-list-refresh {
+  .product-item {
+    .fj();
+    width: 100%;
+    height: 120px;
+    padding: 10px 0;
+    border-bottom: 1px solid #dcdcdc;
+    img {
+      width: 140px;
+      height: 100 px;
+      padding: 0 10px;
+      .boxSizing();
+    }
+    .product-info {
+      width: 56%;
       height: 120px;
-      padding: 10px 0;
-      border-bottom: 1px solid #dcdcdc;
-      img {
-        width: 140px;
-        height: 120px;
-        padding: 0 10px;
-        .boxSizing();
+      padding: 5px;
+      text-align: left;
+      .boxSizing();
+      p {
+        margin: 0;
       }
-      .product-info {
-          width: 56%;
-          height: 120px;
-          padding: 5px;
-          text-align: left;
-          .boxSizing();
-          p {
-            margin: 0
-          }
-          .name {
-            width: 100%;
-            max-height: 40px;
-            line-height: 20px;
-            font-size: 15px;
-            color: #333;
-            overflow: hidden;
-            text-overflow:ellipsis;
-            white-space: nowrap;
-          }
-          .subtitle {
-            width: 100%;
-            max-height: 20px;
-            padding: 10px 0;
-            line-height: 25px;
-            font-size: 13px;
-            color: #999;
-            overflow: hidden;
-          }
-          .price {
-            color: @primary;
-            font-size: 16px;
-          }
+      .name {
+        width: 100%;
+        max-height: 40px;
+        line-height: 20px;
+        font-size: 15px;
+        color: #333;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
       }
+      .subtitle {
+        width: 100%;
+        max-height: 40px;
+        padding: 10px 0;
+        line-height: 25px;
+        font-size: 13px;
+        color: #999;
+        overflow: hidden;
+      }
+      .price {
+        color: @primary;
+        font-size: 16px;
+      }
+    }
   }
   .empty {
     display: block;
