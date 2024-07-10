fac78bbda3282adc0f176da96bc33fb98a93dde8
shiqiwen <1844533830@qq.com>
2024-07-10 17:36:52 +0800

    修改图标、logo，去掉新蜂字样

--- vue3-admin-main/src/views/Login.vue
+++ vue3-admin-main/src/views/Login.vue
@@ -2,9 +2,9 @@
   <div class="login-body">
     <div class="login-container">
       <div class="head">
-        <img class="logo" src="https://s.yezgea02.com/1582958061265/mlogo.png" />
+        <img class="logo" src="/dist/PILIPILI.png" />
         <div class="name">
-          <div class="title">新蜂商城</div>
+          <div class="title">动漫周边商城</div>
           <div class="tips">Vue3.0 后台管理系统</div>
         </div>
       </div>
