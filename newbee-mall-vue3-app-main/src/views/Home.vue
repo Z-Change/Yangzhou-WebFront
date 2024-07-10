714c2531c061ddcb9a2a7bfb2b8e5a596261aba7
shiqiwen <1844533830@qq.com>
2024-07-10 16:10:07 +0800

    把新蜂变成我们的形状了

--- newbee-mall-vue3-app-main/src/views/Home.vue
+++ newbee-mall-vue3-app-main/src/views/Home.vue
@@ -13,9 +13,9 @@
     <header class="home-header wrap" :class="{'active' : state.headerScroll}">
       <router-link tag="i" to="./category"><i class="nbicon nbmenu2"></i></router-link>
       <div class="header-search">
-        <span class="app-name">新蜂商城</span>
+        <span class="app-name">动漫周边商城</span>
         <i class="iconfont icon-search"></i>
-        <router-link tag="span" class="search-title" to="./product-list?from=home">山河无恙，人间皆安
+        <router-link tag="span" class="search-title" to="./product-list?from=home">点我搜索喵❤
         </router-link>
       </div>
       <router-link class="login" tag="span" to="./login" v-if="!state.isLogin">登录</router-link>
@@ -99,13 +99,13 @@ const state = reactive({
   recommends: [],
   categoryList: [
     {
-      name: '新蜂超市',
+      name: '动漫超市',
       imgUrl:
         'https://s.yezgea02.com/1604041127880/%E8%B6%85%E5%B8%82%402x.png',
       categoryId: 100001
     },
     {
-      name: '新蜂服饰',
+      name: '动漫服饰',
       imgUrl:
         'https://s.yezgea02.com/1604041127880/%E6%9C%8D%E9%A5%B0%402x.png',
       categoryId: 100003
@@ -117,13 +117,13 @@ const state = reactive({
       categoryId: 100002
     },
     {
-      name: '新蜂生鲜',
+      name: '非常の新鲜',
       imgUrl:
         'https://s.yezgea02.com/1604041127880/%E7%94%9F%E9%B2%9C%402x.png',
       categoryId: 100004
     },
     {
-      name: '新蜂到家',
+      name: '配送到家',
       imgUrl:
         'https://s.yezgea02.com/1604041127880/%E5%88%B0%E5%AE%B6%402x.png',
       categoryId: 100005
